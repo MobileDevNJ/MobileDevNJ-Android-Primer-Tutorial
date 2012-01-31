7ec157314f30643e30c189c9a7fbc072a9f6d318
David Rodriguez <programmerdave@gmail.com>
2012-01-31 18:17:51 -0500

    Added Readme and added disclosure to eclipse source

--- Eclipse Project/FollowMe/src/com/mobiledevnj/followme/FollowMeActivity.java
+++ Eclipse Project/FollowMe/src/com/mobiledevnj/followme/FollowMeActivity.java
@@ -1,4 +1,17 @@
-package com.mobiledevnj.followme;
+/*
+ * Android Primer Presentation For MobileDevNJ
+ * David Rodriguez
+ * January 2012 Meetup
+ * 
+ * http://www.twitter.com/programmerdave
+ * http://www.github.com/programmerdave
+ * http://www.mobiledevnj.com
+ * http://www.meetup.com/mobiledevnj
+ * 
+ * Feel free to use this code as you wish.
+ */
+ 
+ package com.mobiledevnj.followme;
 
 import android.app.Activity;
 import android.os.Bundle;
---
+++ README.md
@@ -0,0 +1,12 @@
+Android Primer Presentation For MobileDevNJ
+David Rodriguez
+January 2012 Meetup
+
+http://www.twitter.com/programmerdave
+http://www.github.com/programmerdave
+http://www.mobiledevnj.com
+http://www.meetup.com/mobiledevnj
+
+Feel free to use this code as you wish.
+
+Presentation is also included.
